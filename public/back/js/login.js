c04a303dd1c7914435d7c33522f92aff72b47408
xuwei9502 <769347869@qq.com>
2018-12-15 17:02:34 +0800

    login2.0

--- public/back/js/login.js
+++ public/back/js/login.js
@@ -32,6 +32,10 @@ $(function(){
             min:2,
             max:6,
             message:"用户名长度必须是2到6位之间"
+          },
+          // 专门用来定制回调的提示内容的
+          callback:{
+              message:"用户名不存在"
           }
 
         }
@@ -45,16 +49,61 @@ $(function(){
             min:6,
             max:12,
             message:"密码长度必须是6到12位之间"
+          },
+          callback:{
+            message:"密码不正确"
           }
+
         }
       }
 
 
      }
+   })
 
 
+    //3.注册表单校验成功事件，在校验成功时，会触发
+    // 在事件中阻止默认的提交（会跳转），通过ajax进行提交（异步）
+   $('#form').on('success.form.bv',function(e){
+    //  阻止默认的提交
+        e.preventDefault();
+        $.ajax({
+          type:"post",
+          url:"/employee/employeeLogin",
+          dataType:"json",
+          // data:{
+          //   username:username,
+          //   password:password
+          // },
+          data:$('#form').serialize(), //表单序列化
+          success:function(info){
+
+        
+            if(info.success){
+              // 成功跳转到首页
+              location.href="index.html";
+            }
+            if(info.error === 1000){
+              $('#form').data('bootstrapValidator').updateStatus('username',"INVALID","callback")
+            }
+            if(info.error === 1001){
+              $('#form').data("bootstrapValidator").updateStatus("password", "INVALID", "callback")
+            }
+          }
+        })
+   })
      
 
+  //  4.表单重置功能
+   $('[type="reset"]').click(function(){
+  //  重置校验状态
+    // 1.创建实例对象
+    var $Form =  $('#form').data("bootstrapValidator");
+    // 2.调用resetForm()方法重置表单
+    // $Form.resetForm(true);
+    $Form.resetForm();
+    // 如果加参数true说明内容和样式一起重置，这里不用。因为reset本身就是重置按钮
+   })
 
 
 
@@ -64,9 +113,7 @@ $(function(){
 
 
 
-
-
-  })
+  
 
 
 
